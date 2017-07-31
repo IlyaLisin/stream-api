4439f56e80276c728f23ef7b765759fc462c86c4
ilisin <ilisin@life-team.net>
2017-07-31 17:05:28 +0300

    init main and FSM

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"github.com/ryanfaerman/fsm"
+)
+
+type Stream struct {
+	State fsm.State
+
+	// our machine cache
+	machine *fsm.Machine
+}
+
+type Rules struct {
+	rules *fsm.Ruleset
+}
+
+func newRules () *fsm.Ruleset {
+	// Establish some rules for our FSM
+	rules := fsm.Ruleset{}
+	rules.AddTransition(fsm.T{"created", "active"})
+	rules.AddTransition(fsm.T{"active", "interrupted"})
+	rules.AddTransition(fsm.T{"interrupted", "active"})
+	rules.AddTransition(fsm.T{"interrupted", "finished"})
+
+	return &rules
+}
+
+// Add methods to comply with the fsm.Stater interface
+func (t *Stream) CurrentState() fsm.State { return t.State }
+func (t *Stream) SetState(s fsm.State)    { t.State = s }
+
+// A helpful function that lets us apply arbitrary rulesets to this
+// instances state machine without reallocating the machine. While not
+// required, it's something I like to have.
+func (t *Stream) Apply(r *fsm.Ruleset) *fsm.Machine {
+	if t.machine == nil {
+		t.machine = &fsm.Machine{Subject: t}
+	}
+
+	t.machine.Rules = r
+	return t.machine
+}
+
+func main() {
+	var err error
+
+	some_thing := Stream{State: "created"} // Our subject
+	fmt.Println(some_thing)
+
+	rules := newRules()
+
+	err = some_thing.Apply(rules).Transition("active")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(some_thing, "11111111")
+}
